6099d4cc552f4e20f8b71181a7e3fae974fbce2a
ZeroAT <zeroat@live.com>
2018-11-23 21:51:47 -0500

    Accessing data P1

--- congressbot.py
+++ congressbot.py
@@ -2,10 +2,11 @@ import praw
 import requests
 import json
 
-response = requests.get("https://api.propublica.org/congress/v1/115/house/bills/introduced.json", headers={'X-API-Key': 'x2lvYYuxgWmzInlfwUOAqS2LBm7uSpwz8y2aUx7b'})
+response = requests.get("https://api.propublica.org/congress/v1/115/house/bills/introduced.json", headers={'X-API-Key': 'x2lvYYuxgWmzInlfwUOAqS2LBm7uSpwz8y2aUx7b'}).content
 
-print(response.status_code)
 
-if response.status_code == requests.codes.ok:
-    response_native = json.loads(response.text)
-    print(response_native)
+data = json.loads(response)
+
+test = data['results'][0]['bills'][0]['bill_id']
+
+print(test)
