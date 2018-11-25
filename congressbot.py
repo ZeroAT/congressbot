d510a52009222e7154162366ee6f0facd4de5459
ZeroAT <zeroat@live.com>
2018-11-24 21:24:53 -0500

    Add API & status method

--- congressbot.py
+++ congressbot.py
@@ -2,16 +2,29 @@ import praw
 import requests
 import json
 
+def bot_login():
+    print("Using API Info to Login...")
+    reddit = praw.Reddit(client_id='ztpKA8bDHobH6A',
+                         client_secret='AhgsPPtXlUfjQFP-qoWQ5VB8fPk',
+                         username='Zerotil',
+                         password='passmod1',
+                         user_agent='prawguide')
+    return reddit
+
+def get_status():
+    status = data["status"]
+    print("Status: OK")
+
+    return status
+
+
 response = requests.get("https://api.propublica.org/congress/v1/115/house/bills/introduced.json", headers={'X-API-Key': 'x2lvYYuxgWmzInlfwUOAqS2LBm7uSpwz8y2aUx7b'}).content
 
 
 data = json.loads(response)
 
 status = data["status"]
-
-if status == 'OK':
+if get_status() == 'OK':
     for each in data['results'][0]['bills']:
         print(each.get("bill_id"))
-
-test = data['results'][0]['bills'][0]['bill_id']
 
