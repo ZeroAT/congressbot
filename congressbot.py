656957f3c6991c2b2d1422804e7eca23e64df81c
ZeroAT <zeroat@live.com>
2018-11-25 21:57:01 -0500

    Posting to subreddit added

--- congressbot.py
+++ congressbot.py
@@ -20,11 +20,38 @@ def get_status():
 
 response = requests.get("https://api.propublica.org/congress/v1/115/house/bills/introduced.json", headers={'X-API-Key': 'x2lvYYuxgWmzInlfwUOAqS2LBm7uSpwz8y2aUx7b'}).content
 
+def check_if_bill_exists(id):
+    reddit = bot_login()
+
+    for post in reddit.subreddit('congresstests').search(id+":"):
+        print("Searching subreddit for bill...")
+        if post.title:
+            return True
+        else:
+            return False
+
+
+
 
 data = json.loads(response)
+
+reddit = bot_login()
+
 
 status = data["status"]
 if get_status() == 'OK':
     for each in data['results'][0]['bills']:
+
         print(each.get("bill_id"))
+        id = each.get("bill_id")
+        title = each.get("title")
+        url = each.get("congressdotgov_url")
+        post_title = id + ": " + title
+
+        if check_if_bill_exists(id) is True:
+            print("This bill has already been posted.")
+        else:
+            print("Posting to Reddit...")
+            reddit.subreddit('congresstests').submit(post_title, url=url)
+
 
